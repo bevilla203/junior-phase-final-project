b4aad4c6a1c3a5063c58f7c8b194aedc058478f5
bvilla203 <ben.tan.villafranca@gmail.com>
2023-02-14 10:24:40 -0500

    navbar appears correctly... haven't added correct routes yet

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "../pages/Layout"
 /* 
     This is you entry point for your routes
 */
 const Main = () => {
     return (
-        <div>
-           Main 
-        </div>
+        <React.StrictMode>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Layout />} />
+                </Routes>
+            </BrowserRouter>
+        </React.StrictMode>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import {Main} from "./components";
+import { Main } from "./components";
 /* Import and destructure main from src/component/index.js 
 and anything else you may need here */
+
 
 
 const container = document.getElementById("root")
