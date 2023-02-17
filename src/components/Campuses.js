54519940a37069c77ff1e17a5f98735bd2bf0a96
bvilla203 <ben.tan.villafranca@gmail.com>
2023-02-17 12:45:32 -0500

    Formatted and linked campuses to lead to individual campuses

--- src/components/Campuses.js
+++ src/components/Campuses.js
@@ -1,30 +1,32 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchCampusesAsync, selectCampuses } from "../features/Campuses/campusSlice"
-//import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 export default function Campuses() {
   const campuses = useSelector(selectCampuses);
   const dispatch = useDispatch();
     useEffect(() => {
       dispatch(fetchCampusesAsync());
     }, [dispatch]);
+  // unsure as to why activeStyle doesn't work....
+  // I used React Router's website to understand Link
+  // based return statement off of Readium project's AllAuthor's
+  // section
   return (
     <div>
       <h1 className="campusHeader" > List of All Campuses </h1>
       {campuses && campuses.length
         ? campuses.map(campus => (
-            // <NavLink
-            //   to={`/campus/${campus.id}`}
-            //   key={`All campus: ${campus.id}`}
-            // >
-              //
+            <Link
+            to={`/Campuses/${campus.id}`}>
               <div key= {campus.id} className="campus row">
                 <img className = "campusImg" src={campus.imageUrl} />
                 <h2>{campus.name}</h2>
-              </div>
-            // </NavLink>
+            </div>
+            <hr />
+            </Link>
           ))
-        : "Couldn't find any campuses...???"}
+        : "Couldn't find any campuses..."}
     </div>
   );
 }
--- src/components/SingleCampus.js
+++ src/components/SingleCampus.js
@@ -1,18 +1,18 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 import {
   selectSingleCampus,
   fetchSingleCampus,
-} from "../features/SingleCampus/singleCampusSlice"; // must create features/singleCampus/singleCampusSlice
+} from "../features/SingleCampus/singleCampusSlice"; 
 
 export default function SingleCampus() {
   const { campusId } = useParams(); // what does this do? do I need it?
   // useSelector gets a hold of the state SingleCampus which is located in the redux store
   const singleCampus = useSelector(selectSingleCampus);
 
-  // I should destructure singleCampus.info as the parts that make up a campus
   const { name, imageUrl, address, description, students } = singleCampus;
   const dispatch = useDispatch();
 
@@ -25,17 +25,20 @@ export default function SingleCampus() {
       <div id="single-campus-detail" className="row">
         <div className="column mr1">
           <h1>{name}</h1>
+          <img src={imageUrl} />
+          <br />
           <i> {address}</i>
           <p>{description}</p>
         </div>
-        <img src={imageUrl} />
+        <hr />
+        {/* <hr> provides a thematic break, a horizontal line spanning the whole page*/}
         <h2>Student Info:</h2>
         <div>
           {students && students.length
             ? students.map((student, index) => (
                 <div key={student.id}>
                   <p>
-                    {index +1}. Name: {student.firstName} {student.lastName}
+                    {index + 1}. Name: {student.firstName} {student.lastName}
                   </p>
                   <p> Email: {student.email}</p>
                   <br />
@@ -44,7 +47,7 @@ export default function SingleCampus() {
             : "No students are currently attending"}
         </div>
       </div>
-      {/* <hr> provides a thematic break, a horizontal line spanning the whole page*/}
     </div>
   );
 }
+
